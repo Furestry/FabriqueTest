ee3d9f9e2d4e4c94fb426c7e9db48267e7deb454
Sevler <64084536+Furestry@users.noreply.github.com>
2022-02-17 16:17:27 +0300

    Add user - admin:password

--- src/main/java/ru/furestry/fabriquetestapp/configs/WebConfig.java
+++ src/main/java/ru/furestry/fabriquetestapp/configs/WebConfig.java
@@ -1,6 +1,9 @@
 package ru.furestry.fabriquetestapp.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +17,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${admin.password}")
+    private String password;
+
     /**
      * Override configure for security settings
      *
@@ -24,14 +30,27 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(a -> a
-                        .antMatchers("/", "/use**r", "/survey**", "/surveys").permitAll()
+                        .antMatchers("/user**", "/survey**", "/surveys").permitAll()
                         .antMatchers("/admin").authenticated()
                 )
                 .logout(l -> l
-                        .logoutSuccessUrl("/").permitAll()
+                        .logoutSuccessUrl("/surveys").permitAll()
                 )
                 .csrf(c -> c
                         .disable()
                 );
+    }
+
+    /**
+     * Add user admin with configured password
+     *
+     * @param auth auth manager
+     * @throws Exception exception
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(password)
+                .authorities("ROLE_ADMIN");
     }
 }
