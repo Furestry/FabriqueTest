f6ce9ab421c1b32c9d4f6fd88dafc6c9a41f3195
Sevler <64084536+Furestry@users.noreply.github.com>
2022-02-17 17:49:33 +0300

    Added javadoc

--- src/main/java/ru/furestry/fabriquetestapp/entities/Answer.java
+++ src/main/java/ru/furestry/fabriquetestapp/entities/Answer.java
@@ -3,6 +3,11 @@ package ru.furestry.fabriquetestapp.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Class representing a answer
+ *
+ * @author Sevler
+ */
 @Data
 @AllArgsConstructor
 public class Answer {
